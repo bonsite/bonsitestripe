cd855ca9778056807f6c63a587da600525f47edb
CodyKoInABox <codykoinabox@gmail.com>
2024-12-03 21:02:54 -0300

    another fix

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,12 +1,7 @@
 import { publicUrl } from "@/env.mjs";
 import { getTranslations } from "@/i18n/server";
-import AccessoriesImage from "@/images/accessories.jpg";
-import ApparelImage from "@/images/apparel.jpg";
-import { CategoryBox } from "@/ui/category-box";
 import { ProductList } from "@/ui/products/product-list";
-import { YnsLink } from "@/ui/yns-link";
 import * as Commerce from "commerce-kit";
-import Image from "next/image";
 import type { Metadata } from "next/types";
 
 export const metadata = {
